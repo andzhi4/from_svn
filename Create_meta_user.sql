b049644cfe3be775db69bb63b799abf9571614b6
andzhi4 <andzhi4@1a156284-54bc-45be-8e48-530e1939236a>
2016-01-29 10:10:41 +0000

    Added META user creation script

---
+++ Create_meta_user.sql
@@ -0,0 +1,5 @@
+grant connect to META1 identified by password;
+GRANT create view, create materialized view to META1;
+grant resource to META1;
+grant select_catalog_role to META1;
+grant unlimited tablespace to META1;
